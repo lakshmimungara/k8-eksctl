83a22b8b416b7d4bfa8ff2e9328c563932abc2e9
lakshmimungara <lakshmimungara2997@gmail.com>
2024-11-08 10:56:30 +0530

    k8-eksctl

--- helm-installation.sh
+++ helm-installation.sh
@@ -1,5 +1,9 @@
 # Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-echo "helm installation"
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# chmod 700 get_helm.sh
+# ./get_helm.sh
+# echo "helm installation"
+
+curl -fsSL -o helm.tar.gz https://get.helm.sh/helm-v3.16.2-linux-amd64.tar.gz
+tar -zxvf helm.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
