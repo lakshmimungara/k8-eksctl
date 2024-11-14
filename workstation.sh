a45ce85639288b8515f0cc818c78b16a809fefb1
lakshmimungara <lakshmimungara2997@gmail.com>
2024-11-14 20:57:46 +0530

    eks cluster

--- workstation.sh
+++ workstation.sh
@@ -71,10 +71,10 @@ VALIDATE $? "kubens installation"
 
 
 #Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-VALIDATE $? "helm installation"
+sudo curl -fsSL -o helm.tar.gz https://get.helm.sh/helm-v3.16.2-linux-amd64.tar.gz
+sudo tar -zxvf helm.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+
 
 #k9S
 curl -sS https://webinstall.dev/k9s | bash
