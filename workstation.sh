7ef81eec621962c7de12e1e31655f57566b5eb1b
lakshmimungara <lakshmimungara2997@gmail.com>
2024-11-14 20:56:50 +0530

    eks cluster

--- workstation.sh
+++ workstation.sh
@@ -40,10 +40,22 @@ usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
 # eksctl
+# Set architecture and platform
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+# Download eksctl
+echo "Downloading eksctl..."
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# Verify the checksum - oprtional 
+echo "Verifying checksum..."
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+# Extract and move eksctl to /usr/local/bin
+echo "Extracting and moving eksctl..."
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-mv /tmp/eksctl /usr/local/bin
-VALIDATE $? "eksctl installation"
+sudo mv /tmp/eksctl /usr/local/bin
 
 
 # kubectl
